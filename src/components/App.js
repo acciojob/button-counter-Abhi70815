e6bfeb17663d7bda810a0550ece4d6c7dc50935f
Abhi70815 <abhishekchurasia45621@gmail.com>
2024-08-26 22:40:45 +0530

    save

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React,{useState, useEffect} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  function handleBtn(){
+    setCount(count+1);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {count} times</p>
+        <button onClick={handleBtn}>Click me</button>
     </div>
   )
 }
